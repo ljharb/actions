9fbe93b6ca56ba934b9fcada70035fd6e6344f51
Jordan Harband <ljharb@gmail.com>
2020-11-03 12:43:09 -0800

    [actions] run: add node_modules caching

--- node/run/command.sh
+++ node/run/command.sh
@@ -6,35 +6,51 @@ case "${1-:default}" in
     0.*) export NPM_CONFIG_STRICT_SSL=false ;;
 esac
 
-echo "\n\n******> nvm install-latest-npm"
+echo
+echo
+echo "******> nvm install-latest-npm"
 nvm install-latest-npm
 
-export CI_RESET_NODE_VERSION=1
-case "${1-:default}" in
-0.5|0.5.*|0.6|0.6.*|0.7|0.7.*|0.9|0.9.*)
-    echo "\n\n******> nvm install --latest-npm 0.8"
-    nvm install --latest-npm 0.8
-;;
-5.*|6.1|6.2)
-    echo "\n\n******> nvm install --latest-npm 6"
-    nvm install --latest-npm 6
-;;
-9.0|9.1|9.2)
-    echo "\n\n******> nvm install --latest-npm 9"
-    nvm install --latest-npm 9
-;;
-*)
-    export CI_RESET_NODE_VERSION=0
-;;
-esac
+if [ "${3-}" != 'true' ]; then
+    export CI_RESET_NODE_VERSION=1
+    case "${1-:default}" in
+    0.5|0.5.*|0.6|0.6.*|0.7|0.7.*|0.9|0.9.*)
+        echo
+        echo
+        echo "******> nvm install --latest-npm 0.8"
+        nvm install --latest-npm 0.8
+    ;;
+    5.*|6.1|6.2)
+        echo
+        echo
+        echo "******> nvm install --latest-npm 6"
+        nvm install --latest-npm 6
+    ;;
+    9.0|9.1|9.2)
+        echo
+        echo
+        echo "******> nvm install --latest-npm 9"
+        nvm install --latest-npm 9
+    ;;
+    *)
+        export CI_RESET_NODE_VERSION=0
+    ;;
+    esac
 
-echo "\n\n******> npm install"
-npm install
+    echo
+    echo
+    echo "******> npm install"
+    npm install
 
-if [ "${CI_RESET_NODE_VERSION-}" = 1 ]; then
-    echo "\n\n******> nvm use ${1-:default}"
-    nvm use "${1-:default}"
+    if [ "${CI_RESET_NODE_VERSION-}" = 1 ]; then
+        echo
+        echo
+        echo "******> nvm use ${1-:default}"
+        nvm use "${1-:default}"
+    fi
 fi
 
-echo "\n\n******> npm run ${2:-test}"
+echo
+echo
+echo "******> npm run ${2:-test}"
 npm run "${2:-test}"
--- node/run/index.js
+++ node/run/index.js
@@ -6,20 +6,66 @@ const { spawnSync } = require('child_process');
 const path = require('path');
 const semver = require('semver');
 
-const { status: nvmStatus } = spawnSync('node', [require.resolve('setup-node-nvm')]);
+const { status } = spawnSync('node', [require.resolve('setup-node-nvm')]);
 
-if (nvmStatus) {
-  process.exitCode = nvmStatus;
+if (status !== 0) {
+  process.exitCode = status;
   return;
 }
 
-const { status } = spawnSync('bash', [
-  path.join(__dirname, 'command.sh'),
-  core.getInput('node-version'),
-  core.getInput('command'),
-], {
-  cwd: process.cwd(),
-  stdio: 'inherit',
-});
+const cacheKey = core.getInput('cache-node-modules-key');
 
-process.exitCode = status;
+async function main() {
+  let cacheHit = false;
+  if (cacheKey) {
+    process.env.INPUT_KEY = cacheKey;
+    core.getInput('key', { required: true }); // assert
+    process.env.INPUT_PATH = 'node_modules';
+    core.getInput('path', { required: true }); // assert
+
+    const { write } = process.stdout;
+    process.stdout.write = function (arg) {
+      if (typeof arg === 'string') {
+        if (arg.startsWith('::save-state name=')) {
+          const [name, value] = arg.slice('::save-state name='.length).split('::');
+          core.info(`hijacking core.saveState output: ${name.split(',')}=${value}`)
+          name.split(',').forEach((x) => {
+            process.env[`STATE_${x}`] = value;
+          });
+        } else if (arg.startsWith('::set-output name=cache-hit::')) {
+          core.info(`hijacking core.setOutput output: ${arg}`)
+          cacheHit = arg === '::set-output name=cache-hit::true';
+        }
+      }
+      return write.apply(process.stdout, arguments);
+    };
+
+    await require('cache/dist/restore').default();
+  }
+
+  const { status } = spawnSync('bash', [
+    path.join(__dirname, 'command.sh'),
+    core.getInput('node-version', { required: true }),
+    core.getInput('command', { required: true }),
+    String(cacheHit),
+  ], {
+    cwd: process.cwd(),
+    stdio: 'inherit',
+  });
+
+  process.exitCode = status;
+
+  if (status !== 0) {
+    throw status;
+  }
+
+  if (cacheKey) {
+    await require('cache/dist/save').default();
+  }
+}
+main().catch((error) => {
+  if (error) {
+    console.error(error);
+  }
+  if (!process.exitCode) { process.exitCode = 1; }
+});
