56f291b2a0d9e3bce72634356ae949b1e054b9f8
Jordan Harband <ljharb@gmail.com>
2021-06-06 15:53:30 -0700

    [Deps] update `@actions/core`, `@actions/github`, `semver`

--- node/install/post.js
+++ node/install/post.js
@@ -4,7 +4,6 @@ const { spawnSync } = require('child_process');
 const path = require('path');
 
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 const afterSuccess = core.getInput('after_success');
 
