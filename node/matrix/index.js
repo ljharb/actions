67eb15046db29ec065a4ad60f598a54eb918200d
Jordan Harband <ljharb@gmail.com>
2021-10-09 13:40:01 -0700

    [actions] matrix: with a range, ensure proper required/optionals for 0.x and io.js versions

--- node/matrix/index.js
+++ node/matrix/index.js
@@ -15,12 +15,14 @@ async function getNodeVersions(type = 'nodejs') {
 
 const nodeVersions = getNodeVersions();
 
+const allVersions = Promise.all([nodeVersions, getNodeVersions('iojs')]).then(([x, y]) => semver.sort(x.concat(y)).reverse());
+
 function majMin(v) {
 	return `${semver.major(v)}.${semver.minor(v)}`;
 }
 
 function getMinorsByMajor(versions) {
-	const minorEntries = versions.map(v => [`${semver.major(v)}`, majMin(v)]);
+	const minorEntries = versions.map(v => [`${semver.satisfies(v, '< 1') ? majMin(v) : semver.major(v)}`, majMin(v)]);
 	const minorsByMajor = {};
 	minorEntries.forEach(([maj, v]) => {
 		minorsByMajor[maj] = Array.from(new Set([].concat(minorsByMajor[maj] || [], v)));
@@ -34,57 +36,77 @@ function comparator(a, b) {
 	return semver.compare(semver.coerce(b), semver.coerce(a));
 }
 
-async function getPreset(makePayload, preset, type) {
-	let requireds;
-	let optionals;
+function get0xReqs(filter) {
+	const requireds = [
+		'0.12',
+		'0.10',
+		'0.8',
+	].filter(x => !filter || filter.some(f => semver.satisfies(f, x)));
+	const optionals = [
+		'0.11',
+		'0.9',
+		'0.6',
+		//'0.4',
+	].filter(x => !filter || filter.some(f => semver.satisfies(f, x)));
+	return { requireds, optionals };
+}
+
+const iojsRange = '^1 || ^2 || ^3';
+
+function iojsMapper(x) {
+	return semver.satisfies(x, iojsRange) ? `iojs-v${x}` : x;
+}
+
+async function getReqOpts(reqRange, optRange, type) {
+	const versions = (await allVersions).filter(v => (reqRange && semver.satisfies(v, reqRange)) || (optRange && semver.satisfies(v, optRange)));
+	const map = getMinorsByMajor(versions);
+	const { requireds: req0x, optionals: opt0x } = get0xReqs(versions);
+	const values = Object.values(map);
+
+	let requireds = [];
+	let optionals = [];
+	if (type === 'majors') {
+		if (reqRange) {
+			requireds = Object.keys(map).filter(x => semver.subset(x, '>= 1') || req0x.includes(x));
+		}
+		if (optRange) {
+			optionals = values.flat().filter(x => !requireds.includes(x) && opt0x.includes(x));
+		}
+	} else {
+		if (reqRange && !optRange) {
+			const reqs = values.flat();
+			requireds = reqs.filter(x => semver.subset(x, '>= 1') || req0x.includes(x));
+		} else {
+			if (reqRange) {
+				const latest = values.map(([v]) => v);
+				requireds = latest.filter(x => semver.subset(x, '>= 1') || req0x.includes(x));
+			}
+			if (optRange) {
+				const nonLatest = values.flatMap(([, ...vs]) => vs);
+				optionals = nonLatest.filter(x => semver.subset(x, '>= 1') || opt0x.includes(x));
+			}
+		}
+	}
+	requireds.sort(comparator);
+	optionals.sort(comparator);
+
+	requireds = requireds.map(iojsMapper);
+	optionals = optionals.map(iojsMapper).filter(x => !requireds.includes(x));
+
+	return { requireds, optionals };
+}
+
+async function getPreset(preset, type) {
 	if (preset === '0.x') {
 		if (type) {
 			throw new TypeError('the `0.x` preset is incompatible with `type`');
 		}
-		requireds = [
-			'0.12',
-			'0.10',
-			'0.8',
-		],
-		optionals = [
-			'0.11',
-			'0.9',
-			'0.6',
-			//'0.4',
-		];
+		return get0xReqs()
 	} else if (preset === 'iojs') {
-		const iojsVersions = (await getNodeVersions('iojs')).filter(v => semver.satisfies(v, '^1 || ^2 || ^3'));
-		const iojs = getMinorsByMajor(iojsVersions);
-		if (type === 'majors') {
-			requireds = Object.keys(iojs);
-			optionals = [];
-		} else {
-			const values = Object.values(iojs);
-			requireds = values.map(([v]) => v);
-			optionals = values.flatMap(([_, ...vs]) => vs);
-		}
-		requireds = requireds.sort(comparator).map(x => `iojs-v${x}`);
-		optionals = optionals.sort(comparator).map(x => `iojs-v${x}`);
-	} else {
-		const map = getMinorsByMajor((await nodeVersions).filter(v => semver.satisfies(v, preset)));
-		if (type === 'majors') {
-			requireds = Object.keys(map);
-			optionals = [];
-		} else {
-			const values = Object.values(map);
-			requireds = values.map(([v]) => v);
-			optionals = values.flatMap(([_, ...vs]) => vs);
-		}
-		requireds.sort(comparator);
-		optionals.sort(comparator);
+		const iojsVersions = (await getNodeVersions('iojs')).filter(v => semver.satisfies(v, ));
+		return getReqOpts(iojsRange, iojsRange, type);
 	}
-	const requiredsOutput = makePayload(requireds);
-	core.setOutput('requireds', JSON.stringify(requiredsOutput));
-	core.info(`requireds: ${requiredsOutput}`);
-
-	const optionalsOutput = makePayload(optionals);
-	core.setOutput('optionals', JSON.stringify(optionalsOutput));
-	core.info(`optionals: ${optionalsOutput}`);
+	return getReqOpts(preset, preset, type);
 }
 
 async function main() {
@@ -111,33 +133,25 @@ async function main() {
 
 	const makePayload = versionsAsRoot ? (x) => x : (versions) => ({ ...(envs && { envs }), [key]: versions });
 
-	if (preset) {
-		await getPreset(makePayload, preset, type);
-	} else {
-		if (!semver.validRange(requireds) || !semver.validRange(optionals)) {
-			throw new TypeError('`requireds` and `optionals` must both be valid semver ranges');
-		}
 
-		const versions = await getNodeVersions().then((versions) => {
-			if (type === 'majors') {
-				return [...new Set(versions.filter(v => semver.major(v)))];
-			}
-			return [...new Set(versions.filter((v) => `${majMin(v)}`))]
-		});
-
-		const requiredsOutput = makePayload(versions.filter(v => semver.intersects(semver.coerce(v).version, requireds)));
-		core.setOutput('requireds', JSON.stringify(requiredsOutput));
-		core.info(`requireds: ${requiredsOutput}`);
-
-		const optionalsOutput = makePayload(versions.filter(v => semver.intersects(semver.coerce(v).version, optionals)));
-		core.setOutput('optionals', JSON.stringify(optionalsOutput));
-		core.info(`optionals: ${optionalsOutput}`);
+	if (!preset && (!semver.validRange(requireds) || !semver.validRange(optionals))) {
+		throw new TypeError('`requireds` and `optionals` must both be valid semver ranges');
 	}
+	const { requireds: reqs, optionals: opts } = await (preset ? getPreset(preset, type) : getReqOpts(requireds, optionals, type));
+
+	const requiredsOutput = makePayload(reqs);
+	core.setOutput('requireds', JSON.stringify(requiredsOutput));
+	core.info(`requireds: ${JSON.stringify(requiredsOutput)}`);
+
+	const optionalsOutput = makePayload(opts);
+	core.setOutput('optionals', JSON.stringify(optionalsOutput));
+	core.info(`optionals: ${JSON.stringify(optionalsOutput)}`);
 
 	// Get the JSON webhook payload for the event that triggered the workflow
 	const payload = JSON.stringify(github.context.payload, null, '\t');
 	console.log(`The event payload: ${payload}`);
 }
 main().catch((error) => {
+	console.error(error)
 	core.setFailed(error.message);
 });
