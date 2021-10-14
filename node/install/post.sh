702a5a81cd566935f246bd498f88d6b9da3eac41
Jordan Harband <ljharb@gmail.com>
2021-10-13 20:27:25 -0700

    [actions] install: remove commented code

--- node/install/post.sh
+++ node/install/post.sh
@@ -11,9 +11,4 @@ if [ -n "${AFTER_SUCCESS}" ]; then
   echo
   echo "******> $AFTER_SUCCESS"
   eval $AFTER_SUCCESS
-# elif [ -f coverage/*.json ]; then
-#     echo
-#     echo
-#     echo "******> bash <(curl -s https://codecov.io/bash) -f coverage/*.json"
-#     bash <(curl -s https://codecov.io/bash) -f coverage/*.json;
 fi
