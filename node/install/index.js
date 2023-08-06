e9c030cd9f5c67fb503ba435fdc18a81ac0d3cef
Jordan Harband <ljharb@gmail.com>
2023-08-06 14:37:40 +1200

    [node] [install]: install `nvm-exec` as well

--- node/install/index.js
+++ node/install/index.js
@@ -42,9 +42,11 @@ async function installNVM() {
 	const latest = await getLatestNVM();
 	const nvmDir = process.env.NVM_DIR || path.join(os.homedir(), '.nvm');
 	await mkdirp(nvmDir);
-	const url = `https://raw.githubusercontent.com/nvm-sh/nvm/${latest}/nvm.sh`;
 
-	return downloadFile(url, path.join(nvmDir, 'nvm.sh'));
+	return Promise.all([
+		downloadFile(`https://raw.githubusercontent.com/nvm-sh/nvm/${latest}/nvm.sh`, path.join(nvmDir, 'nvm.sh')),
+		downloadFile(`https://raw.githubusercontent.com/nvm-sh/nvm/${latest}/nvm-exec`, path.join(nvmDir, 'nvm-exec')),
+	]);
 }
 
 async function main() {
